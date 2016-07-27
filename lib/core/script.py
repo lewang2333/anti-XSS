dc2997562ecb867ba3ac8784402247b2e6ba09fb
lewangbtcc <le.wang@btcc.com>
2016-07-27 12:03:19 +0800

    add Script class

---
+++ lib/core/script.py
@@ -0,0 +1,38 @@
+
+class Script(object):
+    __payload = ''
+    __fromDomain = ''
+    __danger = False
+
+    def __init__(self):
+        pass
+
+    def __init__(self, payload):
+        self.__payload = payload
+
+    def __init__(self, payload, fromDomain):
+        self.__payload = payload
+        self.__fromDomain = fromDomain
+
+    def __init__(self, payload, fromDomain, danger):
+        self.__payload = payload
+        self.__fromDomain = fromDomain
+        self.__danger = danger
+
+    def setPayload(payload):
+        self.__payload = payload
+
+    def setFromDomain(fromDomain):
+        self.__fromDomain = fromDomain
+
+    def setDanger(danger):
+        self.__danger = danger
+
+    def getPayload(self):
+        return self.__payload
+
+    def getFromDomain(self):
+        return self.__fromDomain
+
+    def getDanger(self):
+        return self.__danger
