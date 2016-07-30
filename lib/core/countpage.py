d2e9289167b538fe5ef83edcbfce3d5f023de088
lewangbtcc <le.wang@btcc.com>
2016-07-30 16:52:01 +0800

    Modify CountPage to a public class

--- lib/core/countpage.py
+++ lib/core/countpage.py
@@ -6,16 +6,16 @@ Copyright (c) 2016 anti-XSS developers
 
 class CountPage(object):
 
-    __number = 0
+    number = 0
 
     def __init__(self, number=0):
-        self.__number = number
+        self.number = number
 
     def setNumber(self, number):
-        self.__number = number
+        self.number = number
 
     def getNumber(self):
-        return self.__number
+        return self.number
 
     def incNumber(self):
-        self.__number += 1
+        self.number += 1
