f6728a714b80907002c5478db2bdd5ace4075523
Le Wang <le.wang@btcc.com>
2016-09-19 14:52:58 +0800

    modify engine

--- application/core/engine.py
+++ application/core/engine.py
@@ -1,6 +1,25 @@
-from models.page import Page
+import urllib2
 
+# from application.models.page import Page
 from urlparse import urlparse
+
+class Page(object):
+    '''
+    A class to store the page
+    '''
+
+    url = ''
+    html = ''
+
+    def __init__(self, url='', html=''):
+        self.url = url
+        self.html = html
+
+        pass
+
+    def get_html(self):
+        return self.html
+
 
 def scan(row_url):
     '''
@@ -10,14 +29,14 @@ def scan(row_url):
     # formalize
     url_str = add_protocol(row_url)
 
-    # BFS
+    # BFS\
     urls = []
     urls.append(url_str)
 
-    for new_url in urls:
-        html = get_html(new_url)
-        page = Page(url=new_url, html=html)
-        urls.extend(page)
+    print url_str
+
+    for temp_url in urls:
+        urls.extend(find_url(temp_url))
 
     return result_html
 
@@ -29,7 +48,7 @@ def add_protocol(row_url):
     if (row_url.lower().strip().find('http://') == -1) and (row_url.lower().strip().find('https://') == -1):
         return 'http://' + row_url.lower().strip()
 
-    pass
+    return row_url
 
 def get_html(url):
     '''
@@ -40,24 +59,33 @@ def get_html(url):
     response = urllib2.urlopen(req)
     html = response.read()
 
+    # print html
     return html
 
-def find_url(page):
+def find_url(url):
     '''
     Find all URLs in a page
     '''
 
     urls = []
-    html = page.get_html()
-    url = page.get_url()
+    html = get_html(url)
     length = len(html)
     front = 0
 
+    flag = False
     while (front <= length):
-        if html.find('<a href=',front) == -1:
-            continue
-        tail = html.find('a>',front)
+        if html.find('<a href=\"',front) == -1:
+            break
+        flag = True
+        tail = html.find('\"',front + 12)
         link = html[html.find('<a href=',front) + 9: tail]
         urls.append(link)
+        print link
+        front = tail
+        if not flag:
+            break
 
+    print urls
     return urls
+
+scan('https://www.btcc.com/news')
